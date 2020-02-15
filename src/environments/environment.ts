c567c1c4dedf86845356ee586fd45e331ce59e2e
Vladlan <lanvlad911@gmail.com>
2020-02-16 00:43:23 +0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBD5VJ7xwR3O11yVt4z4WaTNtKUvqUbr00',
+    authDomain: 'angular-ngrx-8949f.firebaseapp.com',
+    databaseURL: 'https://angular-ngrx-8949f.firebaseio.com',
+    projectId: 'angular-ngrx-8949f',
+    storageBucket: 'angular-ngrx-8949f.appspot.com',
+    messagingSenderId: '359366508952',
+    appId: '1:359366508952:web:1fb7cab203ce3ad4b4ebed'
+  }
 };
 
 /*
